0eaac6b095df35535d735ed6fb0bf9401e53eda4
Elif Boncuk <elifboncuk88@gmail.com>
2019-02-12 23:00:08 +0300

    Initial commit
    (cherry picked from commit e6660147de64ef9c670d261ae7fef724bfab2e7f)

--- app/src/main/java/elifbon/com/MainActivity.kt
+++ app/src/main/java/elifbon/com/MainActivity.kt
@@ -1,63 +1,12 @@
 package elifbon.com
 
-import android.content.Intent
-import android.net.Uri
 import android.os.Bundle
-import android.support.v4.app.ShareCompat
 import android.support.v7.app.AppCompatActivity
-import android.util.Log
-import android.view.View
-import kotlinx.android.synthetic.main.activity_main.*
-
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-    }
-
-    fun openWebsite(view:View){
-
-        var url:String = website_edittext.text.toString()
-
-        var webpage: Uri = Uri.parse(url)
-
-        val intent = Intent(Intent.ACTION_VIEW, webpage)
-
-        if (intent.resolveActivity(getPackageManager()) != null) {
-            startActivity(intent)
-        }else {
-            Log.d("ImplicitIntents", "Can't handle this!");
-        }
-
-    }
-
-    fun openLocation(view:View){
-        val loc = location_edittext.text.toString()
-
-        val addressUri = Uri.parse("geo:0,0?q=$loc")
-
-        val intent = Intent(Intent.ACTION_VIEW, addressUri)
-
-        if (intent.resolveActivity(getPackageManager()) != null) {
-            startActivity(intent);
-        } else {
-            Log.d("ImplicitIntents", "Can't handle this intent!");
-        }
-
-    }
-
-    fun shareText(view:View){
-
-        val txt = share_edittext.text.toString()
-        val mimeType = "text/plain"
-
-        ShareCompat.IntentBuilder
-            .from(this)
-            .setType(mimeType)
-            .setChooserTitle(getString(R.string.share_title))
-            .setText(txt)
-            .startChooser();
     }
 }
