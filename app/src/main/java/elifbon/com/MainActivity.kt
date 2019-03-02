7aad3f4195007b80ff599964e260bea7e79b3201
Elif Boncuk <elifboncuk88@gmail.com>
2019-03-02 23:41:43 +0300

    Add Task 3: Send data from the main Activity to the second Activity

--- app/src/main/java/elifbon/com/MainActivity.kt
+++ app/src/main/java/elifbon/com/MainActivity.kt
@@ -5,20 +5,34 @@ import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
 import android.util.Log
 import android.view.View
+import android.widget.EditText
+
+
 
 class MainActivity : AppCompatActivity() {
 
     private val LOG_TAG = MainActivity::class.java.simpleName
 
+
+    companion object {
+        val EXTRA_MESSAGE = "elifbon.com.extra.MESSAGE"
+    }
+
+    lateinit var mMessageEditText: EditText
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        mMessageEditText = findViewById(R.id.editText_main);
     }
 
     fun launchSecondActivity(view:View){
         Log.d(LOG_TAG, "Button clicked!");
 
         val intent = Intent(this, SecondActivity::class.java)
+        val message = mMessageEditText.text.toString()
+        intent.putExtra(EXTRA_MESSAGE, message);
         startActivity(intent)
     }
 }
--- app/src/main/java/elifbon/com/SecondActivity.kt
+++ app/src/main/java/elifbon/com/SecondActivity.kt
@@ -2,11 +2,18 @@ package elifbon.com
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_second.*
+
 
 class SecondActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_second)
+
+        val intent = intent
+        val message = intent.getStringExtra(MainActivity.EXTRA_MESSAGE)
+
+        text_message.setText(message);
     }
 }
