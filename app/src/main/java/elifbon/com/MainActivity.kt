53a008de7b1917cc7d6bdfd2f2f6f181215e3dc8
Elif Boncuk <elifboncuk88@gmail.com>
2019-03-02 22:32:56 +0300

    Add Task 1: Create the TwoActivities project

--- app/src/main/java/elifbon/com/MainActivity.kt
+++ app/src/main/java/elifbon/com/MainActivity.kt
@@ -1,37 +1,19 @@
 package elifbon.com
 
 import android.os.Bundle
-import android.support.design.widget.Snackbar
 import android.support.v7.app.AppCompatActivity
-import android.view.Menu
-import android.view.MenuItem
-import kotlinx.android.synthetic.main.activity_main.*
+import android.util.Log
 
 class MainActivity : AppCompatActivity() {
 
+    private val LOG_TAG = MainActivity::class.java.simpleName
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        setSupportActionBar(toolbar)
-
     }
 
-    override fun onCreateOptionsMenu(menu: Menu): Boolean {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        menuInflater.inflate(R.menu.menu_main, menu)
-        return true
+    fun launchSecondActivity(){
+        Log.d(LOG_TAG, "Button clicked!");
     }
-
-    override fun onOptionsItemSelected(item: MenuItem): Boolean {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        return when (item.itemId) {
-            R.id.action_settings -> true
-            else -> super.onOptionsItemSelected(item)
-        }
-    }
-
-
 }
