4a35cbaaafbce570e7a69a96ed72fd88dd4f585e
Elif Boncuk <elifboncuk88@gmail.com>
2019-03-03 00:13:10 +0300

    Add Task 4: Return data back to the main Activity

--- app/src/main/java/elifbon/com/SecondActivity.kt
+++ app/src/main/java/elifbon/com/SecondActivity.kt
@@ -1,11 +1,20 @@
 package elifbon.com
 
+import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 import kotlinx.android.synthetic.main.activity_second.*
 
 
+
+
 class SecondActivity : AppCompatActivity() {
+
+    companion object {
+        val EXTRA_MESSAGE = "elifbon.com.extra.REPLY"
+    }
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -15,5 +24,14 @@ class SecondActivity : AppCompatActivity() {
         val message = intent.getStringExtra(MainActivity.EXTRA_MESSAGE)
 
         text_message.setText(message);
+
+    }
+
+    fun returnReply(view:View){
+        val reply = reply_text.getText().toString()
+        val intent:Intent = Intent()
+        intent.putExtra(EXTRA_MESSAGE, reply)
+        setResult(RESULT_OK,intent);
+        finish()
     }
 }
