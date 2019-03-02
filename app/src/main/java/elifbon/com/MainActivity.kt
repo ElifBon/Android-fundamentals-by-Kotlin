9d6fe669a42d71d66d59b72b39c0464e13b95ea4
Elif Boncuk <elifboncuk88@gmail.com>
2019-03-03 00:13:10 +0300

    Add Task 4: Return data back to the main Activity

--- app/src/main/java/elifbon/com/MainActivity.kt
+++ app/src/main/java/elifbon/com/MainActivity.kt
@@ -1,12 +1,13 @@
 package elifbon.com
 
+import android.app.Activity
 import android.content.Intent
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
 import android.util.Log
 import android.view.View
 import android.widget.EditText
-
+import kotlinx.android.synthetic.main.activity_main.*
 
 
 class MainActivity : AppCompatActivity() {
@@ -16,6 +17,8 @@ class MainActivity : AppCompatActivity() {
 
     companion object {
         val EXTRA_MESSAGE = "elifbon.com.extra.MESSAGE"
+
+        val TEXT_REQUEST = 1
     }
 
     lateinit var mMessageEditText: EditText
@@ -33,6 +36,24 @@ class MainActivity : AppCompatActivity() {
         val intent = Intent(this, SecondActivity::class.java)
         val message = mMessageEditText.text.toString()
         intent.putExtra(EXTRA_MESSAGE, message);
-        startActivity(intent)
+        startActivityForResult(intent, TEXT_REQUEST)
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        when(requestCode){
+            TEXT_REQUEST -> getResult(resultCode, data)
+        }
+    }
+
+    private fun getResult(resultCode: Int, data: Intent?) {
+        if(resultCode == Activity.RESULT_OK ){
+            val reply = data?.getStringExtra(SecondActivity.EXTRA_MESSAGE)
+            if(!reply.isNullOrBlank()) {
+                text_header_reply.setVisibility(View.VISIBLE)
+                text_message_reply.setText(reply)
+                text_message_reply.setVisibility(View.VISIBLE)
+            }
+        }
     }
 }
