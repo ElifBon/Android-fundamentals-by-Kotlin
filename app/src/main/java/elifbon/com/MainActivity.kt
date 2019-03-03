e4066a56c71b68264770a1f59391ff3ae232e69a
Elif Boncuk <elifboncuk88@gmail.com>
2019-03-03 19:31:07 +0300

    Initial commit
    (cherry picked from commit e6660147de64ef9c670d261ae7fef724bfab2e7f)
    
    Add Task 5: Receive an implicit Intent

--- app/src/main/java/elifbon/com/MainActivity.kt
+++ app/src/main/java/elifbon/com/MainActivity.kt
@@ -2,11 +2,20 @@ package elifbon.com
 
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
+import kotlinx.android.synthetic.main.activity_main.*
+
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val intent = intent
+        val uri = intent.data
+        if (uri != null) {
+            val uri_string =  uri.toString()
+            text_uri_message.setText(uri_string)
+        }
     }
 }
