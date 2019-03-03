64ca4bf9138c040e632dd041699951a97611e73d
Elif Boncuk <elifboncuk88@gmail.com>
2019-03-03 22:04:33 +0300

    Add Task 2: Implement the layout and MainActivity

--- app/src/main/java/elifbon/com/MainActivity.kt
+++ app/src/main/java/elifbon/com/MainActivity.kt
@@ -2,20 +2,51 @@ package elifbon.com
 
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
+import android.view.View
 import kotlinx.android.synthetic.main.activity_main.*
 
-
 class MainActivity : AppCompatActivity() {
+
+    private val mColorArray = arrayOf(
+        "red",
+        "pink",
+        "purple",
+        "deep_purple",
+        "indigo",
+        "blue",
+        "light_blue",
+        "cyan",
+        "teal",
+        "green",
+        "light_green",
+        "lime",
+        "yellow",
+        "amber",
+        "orange",
+        "deep_orange",
+        "brown",
+        "grey",
+        "blue_grey",
+        "black"
+    )
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        val intent = intent
-        val uri = intent.data
-        if (uri != null) {
-            val uri_string =  uri.toString()
-            text_uri_message.setText(uri_string)
+        // restore saved instance state (the text color)
+        if (savedInstanceState != null) {
+            hello_textview.setTextColor(savedInstanceState.getInt("color"));
         }
+    }
+
+    fun changeColor(view: View){
+
+    }
+
+    public override fun onSaveInstanceState(outState: Bundle) {
+        super.onSaveInstanceState(outState)
+        // save the current text color
+        outState.putInt("color", hello_textview.getCurrentTextColor())
     }
 }
