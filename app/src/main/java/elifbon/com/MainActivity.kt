52ec6410948d71875faed45342e8981883a67403
Elif Boncuk <elifboncuk88@gmail.com>
2019-03-03 16:25:20 +0300

    Add Task 2: Implement the Open Website button

--- app/src/main/java/elifbon/com/MainActivity.kt
+++ app/src/main/java/elifbon/com/MainActivity.kt
@@ -1,8 +1,13 @@
 package elifbon.com
 
+import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
+import android.util.Log
 import android.view.View
+import kotlinx.android.synthetic.main.activity_main.*
+
 
 class MainActivity : AppCompatActivity() {
 
@@ -11,15 +16,27 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
     }
 
-    public fun openWebsite(view:View){
+    fun openWebsite(view:View){
+
+        var url:String = website_edittext.text.toString()
+
+        var webpage: Uri = Uri.parse(url)
+
+        val intent = Intent(Intent.ACTION_VIEW, webpage)
+
+        if (intent.resolveActivity(getPackageManager()) != null) {
+            startActivity(intent)
+        }else {
+            Log.d("ImplicitIntents", "Can't handle this!");
+        }
 
     }
 
-    public fun openLocation(view:View){
+    fun openLocation(view:View){
 
     }
 
-    public fun shareText(view:View){
+    fun shareText(view:View){
 
     }
 }
